dc2c8a8bac5bfc313bb20e3956f261f4160f31c9
liam <unity3d@wooriline.com>
2021-04-22 10:59:35 +0900

    first

---
+++ Assets/Problem1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Problem1 : MonoBehaviour
+{
+   
+    
+
+
+
+
+    public IEnumerator MoveRoutine(){
+        yield break;
+    }
+
+}
