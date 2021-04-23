770ef49899d3cdd74e223b3a53ba49a70465ba86
RankGameDiver <hoo2018@naver.com>
2021-04-23 12:31:53 +0900

    update

--- Assets/Problem1.cs
+++ Assets/Problem1.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class Problem1 : MonoBehaviour
 {
-   
-    
+    /// <summary>
+    /// 
+    /// 이동하는 오브젝트의 수, 딜레이와 이동시간의 비율, 총 이동시간을 가지고 오브젝트 하나의 이동시간과 딜레이를 구하는 코드 작성
+    /// 
+    /// </summary>
+    /// <param name="objCount">이동할 물체의 수</param>
+    /// <param name="moveRatio">(딜레이 + 이동시간)에서 이동시간이 차지하는 비율</param>
+    /// <param name="totalTime">총 이동 시간</param>
+    /// <returns></returns>
 
+    [TestMethod]
+    public void Solution(int objCount = 5, float moveRatio = 0.8f, float totalTime = 1f)
+    {
 
-
-
-    public IEnumerator MoveRoutine(){
-        yield break;
     }
-
 }
