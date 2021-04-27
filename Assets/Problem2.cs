bd01419d5672ee9b7124af38fae17a216ae7aed2
RankGameDiver <hoo2018@naver.com>
2021-04-27 10:01:56 +0900

    Problem2 info update

--- Assets/Problem2.cs
+++ Assets/Problem2.cs
@@ -13,6 +13,8 @@ public class Problem2 : MonoBehaviour
     /// - 시작할때 숫자 폼이 롤링중에도 유지되어야 함
     /// 예) 5K -> 8K일때는 소수점 밑을 표시하지 않음. 5.55k -> 7.89K일때는 소수점 아래 두자릿수까지 롤링되도록 구현(롤링중에 6K, 7K가 되는 상황에서도 6.00K, 7.00K 이런식으로 롤링되도록)
     /// 
+    /// 숫자 롤링 시간은 임의로 지정해서 구현
+    /// 
     /// </summary>
 
     public void Solution(long startNum, long endNum)
