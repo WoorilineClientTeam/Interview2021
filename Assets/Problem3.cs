a7fac6d1b44c4979e9374dbe9836144d95eaa4a5
RankGameDiver <hoo2018@naver.com>
2021-04-27 10:14:04 +0900

    Problem3 Add

---
+++ Assets/Problem3.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Problem3 : MonoBehaviour
+{
+	/// <summary>
+	/// 
+	/// 주석처리된 코드를 읽고 로그에 어떤 값이 나올지 맞추고 설명.
+	/// 
+	/// </summary>
+
+	/*
+	class A
+	{
+		public int number;
+	}
+	struct B
+	{
+		public int number;
+	}
+
+	List<A> aList = new List<A>();
+	List<B> bList = new List<B>();
+
+	public void TestStart()
+	{
+		var a = new A();
+		a.number = 0;
+		var b = new B();
+		b.number = 0;
+
+		for (int i = 0; i < 5; i++)
+		{
+			aList.Add(a);
+			bList.Add(b);
+		}
+
+		var a1 = aList[0];
+		var b1 = bList[0];
+		a1.number = 1;
+		b1.number = 1;
+		//Q : 무엇이 출력될것인가? 결과를 설명하라.
+		Debug.Log(aList[4].number);
+		Debug.Log(bList[4].number);
+	}
+	*/
+}
